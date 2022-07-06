37ec14d01685a462fb2c7653531a6a45a3a3dbcc
TTpeach1 <TTpeach1@Outlook.com>
2022-07-06 21:27:08 +0800

    学生渲染

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,18 @@
   <div>
     <!-- 注册页面 -->
       <MyPhone></MyPhone>
-  </div>
+      <hr>
+    <!-- 学生渲染 -->
+      <MyStudent></MyStudent>
+  </div>  
 </template>
 <script>
 import MyPhone from '@/views/MyPhone';
+import MyStudent from '@/views/MyStudent'
 export default {
   components: {
     MyPhone,
+    MyStudent,
   },
 };
 </script>
---
+++ src/views/MyStudent.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <MS>
+        <template v-slot:id='scope'>
+        <p>{{scope.row+1}}</p>
+      </template>
+      <template v-slot:name='scope'>
+        <input type="text" v-model="scope.row.name">
+      </template>
+      <template v-slot:age='scope'>
+        <input type="text" v-model="scope.row.age">
+      </template>
+      <template v-slot:headImgUrl='scope'>
+        <img :src="scope.row.headImgUrl" alt="" >
+      </template>
+    </MS>
+  </div>
+</template>
+
+<script>
+import MS from '@/components/MS'
+export default {
+    components:{
+        MS,
+    }
+}
+</script>
+
+<style>
+
+</style>
